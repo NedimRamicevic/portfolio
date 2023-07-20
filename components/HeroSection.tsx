eac9689a1746be40aa041cc05da288bdf754c78b
Nedim <mr.ramicevic@gmail.com>
2023-07-20 17:23:44 +0300

    herosection

---
+++ components/HeroSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div>
+            <div>
+                <Image src="" alt='' />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
