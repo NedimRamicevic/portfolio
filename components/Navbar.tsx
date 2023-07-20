30dece3cba09792722c35955b38da84205e57a94
Nedim <mr.ramicevic@gmail.com>
2023-07-20 16:48:09 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
+interface NavItems {
+    name: string
+    href: string
+    label: string
+}
+
+const navItems: NavItems[] = [
+    {
+        name: 'home',
+        href: '/',
+        label: 'Home',
+    },
+    {
+        name: 'my-projects',
+        href: '/my-projects',
+        label: 'My Projects',
+    },
+    {
+        name: 'about',
+        href: '/about',
+        label: 'About',
+    },
+
+]
+
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className=' mx-auto w-full px-4 shadow bg-white fixed top-0 z-50 dark:border-stone-500'>
+        <div className=' justify-between md:items-center md:flex text-black'>
+            <div className=' md:py-5 md:block'>
+                <h2 className=' text-2xl font-bold'>Ramicevic</h2>
+            </div>
+            <div className=' md:flex space-x-4'>
+                {navItems.map((item) => (
+                    <a key={item.name} href={item.href}>
+                        {item.label}
+                    </a>
+                ))    
+                }
+            </div>
+        </div>
+    </header>
   )
 }
 
