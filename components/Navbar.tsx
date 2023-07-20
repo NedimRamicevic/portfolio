dd9e397c45cf38a5b18ec14ae9e58986bddead4c
Nedim <mr.ramicevic@gmail.com>
2023-07-20 18:42:34 +0300

    projects

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,99 @@
-import React from 'react'
+"use client" // this is a client component
+import React from "react"
+import { useState } from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
-interface NavItems {
-    name: string
-    href: string
-    label: string
+interface NavItem {
+  label: string
+  page: string
 }
 
-const navItems: NavItems[] = [
-    {
-        name: 'home',
-        href: '/',
-        label: 'Home',
-    },
-    {
-        name: 'my-projects',
-        href: '/my-projects',
-        label: 'My Projects',
-    },
-    {
-        name: 'about',
-        href: '/about',
-        label: 'About',
-    },
-
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    page: "/",
+  },
+  {
+    label: "About",
+    page: "/about",
+  },
+  {
+    label: "Projects",
+    page: "/my-projects",
+  },
 ]
 
-
-const Navbar = () => {
+export default function Navbar() {
+  const { systemTheme, theme, setTheme } = useTheme()
+  const currentTheme = theme === "system" ? systemTheme : theme
+  const pathname = usePathname()
+  const [navbar, setNavbar] = useState(false)
   return (
-    <header className=' mx-auto w-full px-4 shadow bg-white fixed top-0 z-50 dark:border-stone-500'>
-        <div className=' justify-between md:items-center md:flex text-black'>
-            <div className=' md:py-5 md:block'>
-                <h2 className=' text-2xl font-bold'>Ramicevic</h2>
+    <header className="w-full mx-auto   px-4 sm:px-20 fixed top-0 z-50 shadow duration-700 bg-white text-black dark:text-white dark:bg-black dark:border-b dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <Link href="/">
+              <div className="container flex items-center space-x-2">
+                <h2 className="text-2xl font-bold">NUMUNE</h2>
+              </div>
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="p-20 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
             </div>
-            <div className=' md:flex space-x-4'>
-                {navItems.map((item) => (
-                    <a key={item.name} href={item.href}>
-                        {item.label}
-                    </a>
-                ))    
-                }
-            </div>
+          </div>
         </div>
+
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    href={item.page}
+                    className=
+                      "block lg:inline-block   hover:text-neutral-500 "
+                    
+                    
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                )
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className=" bg-slate-300 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
-
-export default Navbar
